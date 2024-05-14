41f9ebecb0b8fe8ab8a89a1dd7f3656dbb202e4f
Denis <xzotiktolip@gmail.com>
2024-05-14 19:17:29 +0400

    added path for product_detail

--- denis228/store/urls.py
+++ denis228/store/urls.py
@@ -4,6 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('', views.store, name='store'),
+    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+
 ]
 
 
