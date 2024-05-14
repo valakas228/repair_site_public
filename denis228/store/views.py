6e333c2f607e40bae0c37f16ab6e8e237daef228
Denis <xzotiktolip@gmail.com>
2024-05-14 19:17:08 +0400

    added product_detail views

--- denis228/store/views.py
+++ denis228/store/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def store(request):
     products = Product.objects.all()
-    return render(request, 'store/store.html', {'products':products})
+    return render(request, 'store/product_list.html', {'products':products})
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'store/product_detail.html', {'product':product})
