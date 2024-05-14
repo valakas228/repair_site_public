3f908910584b43906004e3488e6f09bd028d746d
Denis <xzotiktolip@gmail.com>
2024-05-14 19:17:42 +0400

    registered ProductImage model

--- denis228/store/admin.py
+++ denis228/store/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Category, Product, ProductAttribute, RepairService
+from .models import Category, Product, ProductAttribute, RepairService, ProductImage
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(ProductAttribute)
 admin.site.register(RepairService)
+admin.site.register(ProductImage)
