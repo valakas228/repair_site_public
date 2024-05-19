8dc56df06ad4d3a35215bde9c71ad4cd6b6c40e2
Denis <xzotiktolip@gmail.com>
2024-05-19 04:04:31 +0400

    registered new models

--- denis228/store/admin.py
+++ denis228/store/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Category, Product, ProductAttribute, RepairService, ProductImage
+from .models import Category, Product, ProductAttribute, RepairService,Comment, Cart, CartItem
 # Register your models here.
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(ProductAttribute)
 admin.site.register(RepairService)
-admin.site.register(ProductImage)
+admin.site.register(Comment)
+admin.site.register(Cart)
+admin.site.register(CartItem)
