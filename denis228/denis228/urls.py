c814899bb8202d59e394b6b62e36fd351a4a6718
Denis <xzotiktolip@gmail.com>
2024-05-21 04:33:01 +0400

    added path to accounts

--- denis228/denis228/urls.py
+++ denis228/denis228/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('store/', include('store.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
