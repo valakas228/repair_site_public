5852ad7a134e5450c8d42caa85988fbd50d7e9af
Denis <xzotiktolip@gmail.com>
2024-06-14 02:20:14 +0400

    w

---
+++ denis228/accounts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя или Email')
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+
