f049b58765aaac354159503dec003cb579a7d074
Denis <xzotiktolip@gmail.com>
2024-06-14 02:18:36 +0400

    added form for repairrequest

--- denis228/store/forms.py
+++ denis228/store/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Product, RepairService, City
+from .models import Product, RepairService, City, RepairRequest
+from django.utils.translation import gettext_lazy as _
 
 class EstimateForm(forms.Form):
     device = forms.ModelChoiceField(queryset=Product.objects.filter(available=True), label="Название устройства")
@@ -15,3 +16,30 @@ class EstimateForm(forms.Form):
 
 class OrderForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, label='Количество', initial=1)
+
+class CreateRepairRequestForm(forms.ModelForm):
+    class Meta:
+        model = RepairRequest
+        fields = ['contact_name', 'device_type', 'issue_description', 'contact_email', 'contact_phone']
+        labels = {
+            'contact_name': _('Имя клиента'),
+            'device_type': _('Тип устройства'),
+            'issue_description': _('Описание проблемы'),
+            'contact_email': _('Контактный email'),
+            'contact_phone': _('Контактный телефон'),
+        }
+        widgets = {
+            'issue_description': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+        }
+
+
+class AdminResponseForm(forms.ModelForm):
+    class Meta:
+        model = RepairRequest
+        fields = ['response']
+        labels = {
+            'response': _('Ответ администратора'),
+        }
+        widgets = {
+            'response': forms.Textarea(attrs={'rows': 4, 'cols': 40}),
+        }
