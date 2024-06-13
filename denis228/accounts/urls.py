60160769068f5797e4bd6e85ac7d9575763c539a
Denis <xzotiktolip@gmail.com>
2024-06-14 02:20:03 +0400

    added urls for RepairRequest

--- denis228/accounts/urls.py
+++ denis228/accounts/urls.py
@@ -1,11 +1,13 @@
+# accounts/urls.py
+
 from django.urls import path
 from . import views
-from django.conf.urls.static import static
-from django.conf import settings
-
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('profile/', views.profile, name='profile'),
-    path('logout/', views.logout_view, name='logout')
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('logout/', views.logout_view, name='logout'),
+    path('change_password/', views.change_password, name='change_password'),
+    path('edit_repair_request/<int:request_id>/', views.edit_repair_request, name='edit_repair_request'),
+    path('delete_repair_request/<int:request_id>/', views.delete_repair_request, name='delete_repair_request'),
+]
